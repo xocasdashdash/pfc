646713b79d36ac48ebd76cd9555aa6750d797e15
xokas <xokas@xokas-desktop.(none)>
2014-04-01 09:46:58 +0200

    Commit de reorganizacion

--- maqueta/js/principal.js
+++ maqueta/js/principal.js
@@ -1,43 +1,45 @@
-window.onload= function () {
+window.onload = function() {
     var $elemento = $("#cont-basico").html();
-    for (i=0; i<2;i++){
-        $($elemento).appendTo("#cont-basico")  ;  
+    for (i = 0; i < 2; i++) {
+        $($elemento).appendTo("#cont-basico");
     }
 };
-$(function(){
-    $('#actividadModal').modal({
-        keyboard: true,
-        backdrop: "static",
-        show:false
+$(document).ready(function() {
+    $(function() {
+        $('#actividadModal').modal({
+            keyboard: true,
+            backdrop: "static",
+            show: false
 
-    }).on('show.bs.modal', function(){ //subscribe to show method
-        var getNombrefromRow =  $(event.target).data('id');
-        var valorNombre = $(event.target).text();
-        //alert('hola');
-        $('#myModalLabel').text(valorNombre );
+        }).on('show.bs.modal', function() { //subscribe to show method
+            var getNombrefromRow = $(event.target).data('id');
+            var valorNombre = $(event.target).text();
+            //alert('hola');
+            $('#myModalLabel').text(valorNombre);
+        });
     });
-});
 
-$(function(){
-    $('#actividadModalPrincipal').modal({
-        keyboard: true,
-        backdrop: "static",
-        show:false
+    $(function() {
+        $('#actividadModalPrincipal').modal({
+            keyboard: true,
+            backdrop: "static",
+            show: false
 
-    }).on('show.bs.modal', function(){ //subscribe to show method
-        var getNombrefromRow =  $(event.target).data('id');
-        var valorNombre = $(event.target).parent().children('h3').text();
-        //alert('hola');
-        $('#myModalLabel').text(valorNombre );
+        }).on('show.bs.modal', function() { //subscribe to show method
+            var getNombrefromRow = $(event.target).data('id');
+            var valorNombre = $(event.target).parent().children('h3').text();
+            //alert('hola');
+            $('#myModalLabel').text(valorNombre);
+        });
     });
-});
 
- $('#inputFechaDatePicker .input-group.date').datepicker({
-    format: "dd/mm/yyyy",
-    weekStart: 1,
-    todayBtn: true,
-    language: "es",
-    multidate: true,
-    multidateSeparator: ";",
-    todayHighlight: true
+    $('#inputFechaDatePicker .input-group.date').datepicker({
+        format: "dd/mm/yyyy",
+        weekStart: 1,
+        todayBtn: true,
+        language: "es",
+        multidate: true,
+        multidateSeparator: ";",
+        todayHighlight: true
+    });
 });
