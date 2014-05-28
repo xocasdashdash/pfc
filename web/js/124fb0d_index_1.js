70d5ae01138659b5d11b0c2bc79e4c9281e19581
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-05-28 23:12:18 +0200

    Configuracion de assetic y de braincrafted_bootstrap para que no dependan de node

--- web/js/124fb0d_index_1.js
+++ web/js/124fb0d_index_1.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-    var elemento = $("#cont-basico").html();
-    for (i = 0; i < 2; i++) {
-        $(elemento).appendTo("#cont-basico");
-    }
     $(function() {
         $('#actividadModal').modal({
             keyboard: true,
@@ -24,18 +20,20 @@ $(document).ready(function() {
 
         }).on('show.bs.modal', function() { //subscribe to show method
             var getNombrefromRow = $(event.target).data('id');
-            var valorNombre = $(event.target).parent().children('h3').text();
-            $('#myModalLabel').text(valorNombre);
+            $('#modal-title').text($(event.target).closest('.actividad').find('.titulo-actividad').text());
+            $('#modal-abstract').text($(event.target).closest('.actividad').find('.abstract-actividad').text());
+            $('#modal-fecha').text($(event.target).closest('.actividad').find('.fecha-publicidad-actividad').text());
+            $('#modal-image').attr("src",$(event.target).closest('.actividad').find('.imagen-actividad').attr("src"));
         });
     });
 
-    $('#inputFechaDatePicker .input-group.date').datepicker({
-        format: "dd/mm/yyyy",
-        weekStart: 1,
-        todayBtn: true,
-        language: "es",
-        multidate: true,
-        multidateSeparator: ";",
-        todayHighlight: true
-    });
+//    $('#inputFechaDatePicker .input-group.date').datepicker({
+//        format: "dd/mm/yyyy",
+//        weekStart: 1,
+//        todayBtn: true,
+//        language: "es",
+//        multidate: true,
+//        multidateSeparator: ";",
+//        todayHighlight: true
+//    });
 });
