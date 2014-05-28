244afa25047e7ce1ae882fd8d8a98042607b29fe
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-05-29 00:46:06 +0200

    Deshago los cambios que hice para lo de la cache

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,12 +34,5 @@ class AppKernel extends Kernel {
     public function registerContainerConfiguration(LoaderInterface $loader) {
         $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
-    public function getCacheDir()
-    {
-        return 'app/cache/'.$this->environment;
-    }
-    public function getLogDir()
-    {
-        return 'app/logs/'.$this->environment;
-    }
+    
 }
