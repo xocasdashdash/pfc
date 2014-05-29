0f8001772c31c76924a49093de73bb576eea62c0
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-05-29 10:33:59 +0200

    Proyecto adaptado a OpenShift

---
+++ app/config/params.php
@@ -0,0 +1,8 @@
+<?php
+  # app/config/params.php
+  $container->setParameter('database_host', getEnv("OPENSHIFT_MYSQL_DB_HOST"));
+  $container->setParameter('database_port', getEnv("OPENSHIFT_MYSQL_DB_PORT"));
+  $container->setParameter('database_name', getEnv("OPENSHIFT_APP_NAME"));
+  $container->setParameter('database_user', getEnv("OPENSHIFT_MYSQL_DB_USERNAME"));
+  $container->setParameter('database_password', getEnv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+?>
