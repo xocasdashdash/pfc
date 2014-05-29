3b18300494d509fa8255bb74ff664c6686c03173
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-05-29 13:44:02 +0200

    Primera versión del script de instalación

--- ConfiguracionApache/install.sh
+++ ConfiguracionApache/install.sh
@@ -1,9 +1,26 @@
 #!/bin/bash   
-
+APACHE_CONF_DIR=/etc/apache2
 #Incluir hosts en el nuevo archivo
 #Modificar gestor.conf para que cambie el directorio de destino
+cat gestorapacheconfig.conf | sed 's?$DIR_APACHE_WWW?'`pwd`/..'?' > gestor.conf
 #Copia el gestor.conf al sites-available de apache2
+cp gestor.conf gestor
+sudo cp gestor $APACHE_CONF_DIR/sites-available;
+sudo cp gestor.conf $APACHE_CONF_DIR/sites-available;
 #Habilitar el site
+sudo a2dissite gestor*;
+sudo a2ensite gestor*;  
 #Reload apache2
+sudo service apache2 reload;
 #Lanzar composer install
-#hacer el cambio de permisos para los problemas de caché
+cd ..;
+if [ ! -f "../composer.phar" ]; then
+    curl -s https://getcomposer.org/installer | php -- --install-dir=..
+else
+    php ../composer.phar self-update
+fi
+php ../composer.phar install
+#Arreglo la cache
+chmod -R 0777 app/dev/cache
+chmod -R 0777 app/dev/logs
+cd -
--- app/config/params.php
+++ app/config/params.php
@@ -1,8 +1,18 @@
 <?php
-  # app/config/params.php
-  $container->setParameter('database_host', getEnv("OPENSHIFT_MYSQL_DB_HOST"));
-  $container->setParameter('database_port', getEnv("OPENSHIFT_MYSQL_DB_PORT"));
-  $container->setParameter('database_name', getEnv("OPENSHIFT_APP_NAME"));
-  $container->setParameter('database_user', getEnv("OPENSHIFT_MYSQL_DB_USERNAME"));
-  $container->setParameter('database_password', getEnv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+
+# app/config/params.php
+if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR']) || (isset($_SERVER['REMOTE_ADDR']) && in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')))) { 
+    $container->setParameter('database_host', getEnv("OPENSHIFT_MYSQL_DB_HOST"));
+    $container->setParameter('database_port', getEnv("OPENSHIFT_MYSQL_DB_PORT"));
+    $container->setParameter('database_name', getEnv("OPENSHIFT_APP_NAME"));
+    $container->setParameter('database_user', getEnv("OPENSHIFT_MYSQL_DB_USERNAME"));
+    $container->setParameter('database_password', getEnv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+} else {
+    $container->setParameter('database_host', "localhost");
+    $container->setParameter('database_port', "1521");
+    $container->setParameter('database_name', "symfony");
+    $container->setParameter('database_user', "root");
+    $container->setParameter('database_password', "toor");
+    
+}
 ?>
