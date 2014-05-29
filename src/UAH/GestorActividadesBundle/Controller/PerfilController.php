4782fa6d7cd0486f2da9e8a94d2a72184a97cefd
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-05-29 18:25:11 +0200

    Primera versión del perfil del usuario

--- src/UAH/GestorActividadesBundle/Controller/PerfilController.php
+++ src/UAH/GestorActividadesBundle/Controller/PerfilController.php
@@ -5,13 +5,17 @@ namespace UAH\GestorActividadesBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-
+use UAH\GestorActividadesBundle\Entity\User as User;
 class PerfilController extends Controller {
     /**
      * @Route("/perfil")
      * @Method({"GET"})
      */
-    public function indexAction() {      
+    public function indexAction() {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $degree = $user->getDegreeId();
+        return $this->render('UAHGestorActividadesBundle:Perfil:index.html.twig', array('user' => $user,
+            'degree' => $degree,));
     }
     /**
      * @Route("/perfil")
