b47abf877751bc2af26591d67d2dba64e7afbbc4
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-06-13 19:57:10 +0200

    - Ahora si un usuario no esta correctamente logueado el servidor devuelve un mensaje de error al intentar inscribirse por AJAX - Cambio de semántica en un par de entidades - Tengo que echarle un ojo a applicationform para que quede coherente con el resto

--- src/UAH/GestorActividadesBundle/Controller/LoginController.php
+++ src/UAH/GestorActividadesBundle/Controller/LoginController.php
@@ -21,19 +21,14 @@ class LoginController extends Controller {
      * Route("/login/{after_login}/{openid_identifier}", defaults={"after_login" = " ","openid_identifier" = " " })
      * @Route("/login")
      */
-    public function loginAction(Request $request){//, $after_login, $openid_identifier) {
-//        if ($openid_identifier === " " &&
-//                $this->container->hasParameter("default_openid_connector")) {
-//            $openid_identifier = $this->container->getParameter("default_openid_connector");
-//        } else {
-//            throw new RuntimeException("Falta configurar el parametro default_openid_connector");
-//        }
-//        if ($after_login === " " &&
-//                $this->container->hasParameter("default_redirect_after_login")) {
-//            $after_login = $this->container->getParameter("default_redirect_after_login");
-//        } else {
-//            throw new RuntimeException("Falta configurar el parametro default_redirect_after_login");
-//        }
+    public function loginAction(Request $request){
+        
+        if($request->isXmlHttpRequest() && !$this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')){
+            $respuesta = array();
+            $respuesta['message'] = 'NOT_LOGGED_IN';
+            $respuesta['status'] = 'error';
+            return new \Symfony\Component\HttpFoundation\JsonResponse($respuesta,401);
+        }
         $openid_identifier = $this->container->getParameter("default_openid_connector");
         return $this->redirect($this->generateUrl("fp_openid_security_check", array('openid_identifier' => $openid_identifier), "301"));
         /* ,/*'_target_path' => $after_login) */
