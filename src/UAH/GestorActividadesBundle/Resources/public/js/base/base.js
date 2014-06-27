d48c4d2517a721f8a9d1c4c5f6697ba9fea41689
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-06-27 22:18:37 +0200

    - Ya puedo seleccionar los datos a enviar - Incluido el primero prototipo de protección por CSRF general

---
+++ src/UAH/GestorActividadesBundle/Resources/public/js/base/base.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function() {
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", $("#csrf-ajax-token").text());
+            }
+        }
+    });
+});
