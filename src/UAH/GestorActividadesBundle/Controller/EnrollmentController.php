67ef17311f7110b88cdc602f16291522f63b9d5a
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-06-29 12:40:42 +0200

    - Se envían bien los datos por cada fila seleccionada - Se podría meter un check que se cancela la acción si no cumple las reglas de validación

--- src/UAH/GestorActividadesBundle/Controller/EnrollmentController.php
+++ src/UAH/GestorActividadesBundle/Controller/EnrollmentController.php
@@ -17,6 +17,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use UAH\GestorActividadesBundle\Repository\EnrollmentRepository;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class EnrollmentController extends Controller {
 
@@ -28,6 +29,7 @@ class EnrollmentController extends Controller {
     //Error cuando no me puedo inscribir en la actividad por su estado
     const ENROLLMENT_ERROR_INVALID_ACTIVITY = 4;
     const ENROLLMENT_ERROR_UNKNOWN = 8;
+    const RECOGNIZEMENT_ERROR_CSRF_TOKEN_INVALID = 1;
 
     /**
      * @Route("/enroll/{activity_id}",requirements={"pagina" = "\d+"}, options={"expose"=true})
@@ -39,14 +41,14 @@ class EnrollmentController extends Controller {
          * 
          * Inscribo al usuario
          */
-        
+
         //Cargo el usuario
         $user = $this->getUser();
         //Compruebo que la actividad tiene huecos libres (places_occupied<places_offered)
         //Compruebo que el usuario no esta ya inscrito en la actividad (Si ya esta, le devuelvo ok)
         $em = $this->getDoctrine()->getManager();
         $valido = $this->get('form.csrf_provider')->isCsrfTokenValid('basico', $request->headers->get('X-CSRFToken'));
-        
+
         //Uso bitmasks para saber que tipo de error hay (si lo hay) 
         $check_enrolled = $em->getRepository('UAHGestorActividadesBundle:Enrollment')->checkEnrolled($user, $activity);
         $free_places = ($activity->getNumberOfPlacesOccupied() >= $activity->getNumberOfPlacesOffered()) << 1;
@@ -81,6 +83,36 @@ class EnrollmentController extends Controller {
             $response = 'Enrolled';
         }
         return new JsonResponse($response, $code);
+    }
+
+    /**
+     * 
+     * @param \UAH\GestorActividadesBundle\Entity\Activity $activity
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * 
+     * @Route("/enroll/recognize/{activity_id}",requirements={"pagina" = "\d+"}, options={"expose"=true})
+     * @ParamConverter("activity", class="UAHGestorActividadesBundle:Activity",options={"id" = "activity_id"})
+     * @Security("(is_granted('edit_activity',activity) && has_role('ROLE_UAH_STAFF_PDI')) || has_role('ROLE_UAH_ADMIN')")
+     */
+    public function recognizeAction(Activity $activity, Request $request) {
+        if ($request->isXmlHttpRequest() && $request->headers->get("X-CSRFToken", null) !== null &&
+                $this->get('form.csrf_provider')->isCsrfTokenValid('administracion', $request->headers->get('X-CSRFToken'))) {
+            $content = json_decode($request->getContent());
+            foreach ($content as $recognizement) {
+                $id = $recognizement->id;
+                $number_of_credits = $recognizement->numero_de_creditos;
+            }
+            return new JsonResponse(true, 200);
+        } else {
+            $response = array();
+            $response['code'] = self::RECOGNIZEMENT_ERROR_CSRF_TOKEN_INVALID;
+            $response['message'] = 'El token CSRF no es válido. Intentalo de nuevo';
+            $response['type'] = 'error';
+            $json_response = new JsonResponse($response, 403);
+            $cookie = new Cookie('X-CSRFToken', $this->get('form.csrf_provider')->generateCsrfToken('administracion'), 0, '/', null, false, false);
+            $json_response->headers->setCookie($cookie);
+            return $json_response;
+        }
     }
 
 }
