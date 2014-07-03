da84719311b8d0d70be243b85146d2c55bc0eda9
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-07-03 20:25:52 +0200

    - Quitado echo de debug del fichero params.php

--- app/config/params.php
+++ app/config/params.php
@@ -2,7 +2,6 @@
 
 # app/config/params.php
 if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR']) || (isset($_SERVER['REMOTE_ADDR']) && !in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))) || getEnv("OPENSHIFT_MYSQL_DB_HOST")) {
-    echo"LOADING";
     $container->setParameter('database_driver', "pdo_mysql");
     $container->setParameter('database_host', getEnv("OPENSHIFT_MYSQL_DB_HOST"));
     $container->setParameter('database_port', getEnv("OPENSHIFT_MYSQL_DB_PORT"));
