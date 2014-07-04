5100aa51286f183015bc0fb79400837a294f110a
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-07-04 18:39:46 +0200

    - El enroll esta en su propio fichero para hacerlo reutilizable facilmente

--- src/UAH/GestorActividadesBundle/Resources/public/js/index.js
+++ src/UAH/GestorActividadesBundle/Resources/public/js/index.js
@@ -39,64 +39,7 @@ $(document).ready(function() {
             }
         });
     });
-    $('.activity').on('click', '.enroll-button', function(event) {
-        var $id = $(event.delegateTarget).data('activity-id');
-        var $boton = $(this);
-//        var $width = $boton.width();
-//        var $height = $boton.height();
-        var $img = $('#ajax-loading-image');
-        //$boton.html('');
-        $boton.html($img.clone());
-        $.ajax({
-            type: "POST",
-            url: Routing.generate('uah_gestoractividades_enrollment_enroll', {activity_id: $id}),
-            statusCode: {
-                200: function(data) {
-                    console.log('Inscrito en la actividad:' + $id);
-                    $boton.addClass('btn-success  already-enrolled').removeClass('btn-primary enroll-button');
-                    $boton.html('<span class="texto">Inscrito!</span><span class="fa fa-check fa-2x"></span>');
-                    $(event.delegateTarget).data('enrolled-in', true);
-                },
-                401: function(data) {
-                    $('#notification').removeClass('hide');
-                    $('#notification').addClass('alert-info');
-                    $('#notification #type').text('Atención');
-                    $('#notification #message').html('Tienes que hacer  <a href="login" class="alert-link">login</a>!');
-                    $boton.html('<span class="texto">Inscribete!</span><span class="glyphicon glyphicon-pencil"></span>');
 
-                },
-                403: function(data) {
-                    console.log('Error al inscribirse');
-                    $boton.html('<span class="texto">Inscribete!</span><span class="glyphicon glyphicon-pencil"></span>');
-
-                    $('#notification').removeClass('hide');
-                    switch (data.responseJSON.type) {
-                        case 'notice':
-                            $('#notification').addClass('alert-info');
-                            $type = 'Notificación';
-                            break;
-                        case 'warning':
-                            $('#notification').addClass('alert-warning');
-                            $type = 'Atención';
-                            break;
-                        case 'error':
-                            $('#notification').addClass('alert-danger');
-                            $type = 'Error';
-                            break;
-                        default:
-                            if (data.status === 401)
-                                $('#notification').addClass('alert-danger');
-                            $type = 'Error';
-
-                            break;
-                    }
-                    $('#notification #type').text($type);
-                    $('#notification #message').text(data.responseJSON.message);
-                }
-            }
-        }
-        );
-    });
     $('#notification').bind('close.bs.alert', function(evt) {
         evt.preventDefault();
         $('#notification').addClass('hide');
