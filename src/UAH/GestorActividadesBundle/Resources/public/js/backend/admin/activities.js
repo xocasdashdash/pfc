7aee9d35ce2f7d1a505fefafc53b7cba7ddf6a70
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-08-10 11:36:52 +0200

    Arreglado problema de actualizar a Symfony 2.5

---
+++ src/UAH/GestorActividadesBundle/Resources/public/js/backend/admin/activities.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).on('ready', function() {
+
+    $('#btn_show_all_activities').on('click', function() {
+
+    });
+    $('#btn_approve_selected').on('click', function() {
+    });
+    $('#btn_print_pending_report').on('click', function() {
+
+    });
+});
