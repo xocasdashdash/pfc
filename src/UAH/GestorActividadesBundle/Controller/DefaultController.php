ff45a8727a971447ac1818fafbcfe243789292c7
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-08-23 17:44:50 +0200

    - Actualizadas dependencias. Quizas habría que quitarlas del repo - Añadido scroll infinito - Pendiente de arreglar que funcione el inscribirse

--- src/UAH/GestorActividadesBundle/Controller/DefaultController.php
+++ src/UAH/GestorActividadesBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace UAH\GestorActividadesBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller {
 
@@ -18,16 +19,31 @@ class DefaultController extends Controller {
         $activity_repository = $em->getRepository('UAHGestorActividadesBundle:Activity');
         $category_repository = $em->getRepository('UAHGestorActividadesBundle:Category');
         $activity_repository->updatePublished();
-        $activities = $activity_repository->getPublishedActivities(); //findBy(array(), array('publicityStartDate' => 'ASC'));
+        $activities = $activity_repository->getPublishedActivities($pagina, 1); //findBy(array(), array('publicityStartDate' => 'ASC'));
         $categories = $category_repository->getFrontPage();
-        $num_activities = count($activities);
-        $enrollments_id = array();
+        $num_activities = $activity_repository->getCountPublishedActivities();
         $enrolled_activities = $em->getRepository('UAHGestorActividadesBundle:Enrollment')->getEnrolledActivitiesId($this->getUser(), $pagina);
         return $this->render('UAHGestorActividadesBundle:Default:index.html.twig', array(
                     'activities' => $activities,
                     'enrollments' => $enrolled_activities,
                     'categories' => $categories,
                     'num_activities' => $num_activities));
+    }
+
+    /**
+     * @Route("/ajax/{page}", requirements={"pagina" = "\d+"}, defaults={"page" = 1},options={"expose"=true}))
+     */
+    public function ajaxActivities($page) {
+        $em = $this->getDoctrine()->getManager();
+        $activity_repository = $em->getRepository('UAHGestorActividadesBundle:Activity');
+        $activities = $activity_repository->getPublishedActivities($page, 1);
+        $enrolled_activities = $em->getRepository('UAHGestorActividadesBundle:Enrollment')->getEnrolledActivitiesId($this->getUser(), $page);
+        $html = $this->renderView('UAHGestorActividadesBundle:Activity:activity-pagination.html.twig', array(
+            'activities' => $activities,
+            'enrollments' => $enrolled_activities));
+        $response = array();
+        $response['html'] = $html;
+        return new JsonResponse($response);
     }
 
 }
