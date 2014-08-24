a715f9c2b0b9d9c36b09c5ec585e47bb2397e942
Joaquín Fernández Campo <jfcampo@gmail.com>
2014-08-24 19:16:13 +0200

    - Ahora en la versión móvil se muestra el botón de Log In/Out en la vista principal - Cambiados algunos javascripts para que dependan del evento window.load y no haya problemas porque no se haya cargado algo

--- src/UAH/GestorActividadesBundle/Form/UserType.php
+++ src/UAH/GestorActividadesBundle/Form/UserType.php
@@ -16,55 +16,66 @@ class UserType extends AbstractType {
         $degrees = $options['degrees'];
         $builder
                 ->add('name', null, array(
-                    'required' =>true,
-                    'label' => 'Nombre', 
+                    'required' => true,
+                    'label' => 'Nombre',
                     'attr' => array(
-                        'help_text' => 'Tu nombre', 
-                        'label_col' => 4, 
-                        'widget_col' => 6, 
+                        'help_text' => 'Tu nombre',
+                        'label_col' => 4,
+                        'widget_col' => 8,
                         'col_size' => 'xs')))
                 ->add('apellido_1', null, array(
-                    'required' =>true,
-                    'label' => 'Primer apellido', 
-                    'attr' => array('label_col' => 4, 'widget_col' => 6, 'col_size' => 'xs')))
+                    'required' => true,
+                    'label' => 'Primer apellido',
+                    'attr' => array(
+                        'label_col' => 4,
+                        'widget_col' => 8,
+                        'col_size' => 'xs')))
                 ->add('apellido_2', null, array(
-                    
-                    'label' => 'Segundo apellido', 
+                    'label' => 'Segundo apellido',
                     'attr' => array(
                         'help_text' => 'Si no tienes un segundo apellido, déjalo en blanco. Si tienes más de dos apellidos pon aquí el segundo y sucesivos',
-                        'label_col' => 4, 
-                        'widget_col' => 6, 
+                        'label_col' => 4,
+                        'widget_col' => 8,
                         'col_size' => 'xs')))
                 ->add('email', null, array(
-                    'required' =>true,
+                    'required' => true,
                     'label' => 'E-mail',
                     'attr' => array(
                         'help_text' => 'Un email que consultes habitualmente',
-                        'label_col' => 4, 'widget_col' => 6, 'col_size' => 'xs')))
+                        'label_col' => 4,
+                        'widget_col' => 8,
+                        'col_size' => 'xs')))
                 ->add('documento_identidad', null, array(
-                    'required' =>true,
-                    'label' => 'Nº DNI/NIE/Pasaporte',
+                    'required' => true,
+                    'label' => 'Nº DNI/ NIE/ Pasaporte',
                     'attr' => array(
                         'help_text' => 'El que tengas registrado en la universidad. Si es DNI o NIE incluye la(s) letra(s)',
-                        'label_col' => 4, 
-                        'widget_col' => 6,
+                        'label_col' => 4,
+                        'widget_col' => 8,
                         'col_size' => 'xs')))
                 ->add('tipo_documento_identidad', 'choice', array('label' => 'Tipo de documento', 'expanded' => true, 'choices' => array(
                         'D.N.I.' => 'D.N.I.',
                         'N.I.E.' => 'N.I.E.',
                         'Pasaporte' => 'Pasaporte'
-                    ), 'attr' => array('help_text' => 'DNI, NIE o Pasaporte', 'label_col' => 4, 'widget_col' => 6, 'col_size' => 'xs',
+                    ), 'attr' => array('help_text' => 'DNI, NIE o Pasaporte',
+                        'label_col' => 4,
+                        'widget_col' => 8,
+                        'col_size' => 'xs',
                         'multiple' => true)))
                 ->add('degree_id', 'entity', array(
-                    'required' =>true,
+                    'required' => true,
                     'class' => 'UAHGestorActividadesBundle:Degree',
                     'choices' => $degrees,
                     'property' => 'name',
                     'group_by' => 'knowledgeArea',
                     'label' => 'Plan de estudio',
-                    'attr' => array('help_text' => 'Elige tu plan de estudio', 'label_col' => 4, 'widget_col' => 6, 'col_size' => 'xs')
+                    'attr' => array(
+                        'help_text' => 'Elige tu plan de estudio',
+                        'label_col' => 4,
+                        'widget_col' => 8,
+                        'col_size' => 'xs')
                 ))
-                ->add('Guardar', 'submit',array('attr' => array('type' => 'default', 'class' => 'btn-block','col_size' => 'xs','label_col' => 7,'widget_col' => 3)))
+                ->add('Guardar', 'submit', array('attr' => array('type' => 'default', 'class' => 'btn-block', 'col_size' => 'xs', 'label_col' => 7, 'widget_col' => 3)))
         ;
     }
 
