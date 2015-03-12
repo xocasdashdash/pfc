9b6e43a11f4818bc29d746b1220fde79a7214268
Joaquín Fernández Campo <jfcampo@gmail.com>
2015-03-12 23:13:31 +0100

    - Modificado el fichero para que cargue las variables de BD desde variables de entorno

--- app/config/params.php
+++ app/config/params.php
@@ -1,13 +1,13 @@
 <?php
 
 # app/config/params.php
+$container->setParameter('database_driver', "pdo_mysql");
 if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR']) || (isset($_SERVER['REMOTE_ADDR']) && !in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))) || getEnv("OPENSHIFT_MYSQL_DB_HOST")) {
-    $container->setParameter('database_driver', "pdo_mysql");
-    $container->setParameter('database_host', getEnv("OPENSHIFT_MYSQL_DB_HOST"));
-    $container->setParameter('database_port', getEnv("OPENSHIFT_MYSQL_DB_PORT"));
-    $container->setParameter('database_name', getEnv("OPENSHIFT_APP_NAME"));
-    $container->setParameter('database_user', getEnv("OPENSHIFT_MYSQL_DB_USERNAME"));
-    $container->setParameter('database_password', getEnv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+    
+    $container->setParameter('database_host', getEnv("MYSQL_DB_HOST"));
+    $container->setParameter('database_port', getEnv("MYSQL_DB_PORT"));
+    $container->setParameter('database_name', getEnv("MYSQL_USER"));
+    $container->setParameter('database_user', getEnv("MYSQL_DATABASE"));
 } else {
     //Configuracion ORACLE
 //    $container->setParameter('database_driver', "oci8");
@@ -18,10 +18,9 @@ if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     //Configuracion MySQL
 
     $container->setParameter('database_host', "localhost");
-    $container->setParameter('database_driver', "pdo_mysql");
-    $container->setParameter('database_port', "3306");
+    $container->setParameter('database_port', "1521");
     $container->setParameter('database_name', "symfony");
-    $container->setParameter('database_user', "");
-    $container->setParameter('database_password', "");
+    $container->setParameter('database_user', "root");
+    $container->setParameter('database_password', "toor");
 }
 $container->setParameter('uploadImagesDir', 'web/upload/images');
