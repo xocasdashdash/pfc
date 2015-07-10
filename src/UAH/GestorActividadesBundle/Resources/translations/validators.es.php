a33b59f3863facf00eeb30ed16fcf6d37ab8cb88
Joaquín Fernández Campo <jfcampo@gmail.com>
2015-07-10 21:11:12 +0200

    - Modificado nombre del fichero de mensajes de validacion para que sea el correspondiente al dominio de traducción

---
+++ src/UAH/GestorActividadesBundle/Resources/translations/validators.es.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return array(
+  'activity.name.not-blank' => 'El nombre no puede estar vacío',
+  'activity.nameEnglish.not-blank' => 'El nombre en inglés no puede estar vacío',
+  'activity.nameEnglish.not-blank' => 'El nombre en inglés no puede estar vacío',
+  'activity.minCreditsECTS.not-blank' => 'El nombre mínimo de créditos ECTS no puede estar vacio',
+  'activity.maxCreditsECTS.not-blank' => 'El número máximo de créditos ECTS no puede estar vacio',
+  'activity.minCredits.not-blank' => 'El número mínimo de créditos no puede estar vacio',
+  'activity.maxCredits.not-blank' => 'El número máximo de créditos no puede estar vacio',
+    ''
+);
